ffaf80a6d9c8328fb1d4698053f9f2db44677757
Achim Friedland <achim.friedland@graphdefined.com>
2023-07-11 21:31:10 +0200

    premerge

--- Vanaheimr Electric/Program.cs
+++ Vanaheimr Electric/Program.cs
@@ -1,21 +1,27 @@
 ﻿
 using Org.BouncyCastle.Asn1.Sec;
-using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Parameters;
+
 using org.GraphDefined.Vanaheimr.Illias;
+
 
 var ecParameters  = SecNamedCurves.GetByName("secp256r1");
 var generator     = GeneratorUtilities.GetKeyPairGenerator("ECDH");
-generator.Init(new ECKeyGenerationParameters(new ECDomainParameters(ecParameters.Curve,
-                                                                    ecParameters.G,
-                                                                    ecParameters.N,
-                                                                    ecParameters.H,
-                                                                    ecParameters.GetSeed()),
-                                             new SecureRandom()));
+generator.Init(new ECKeyGenerationParameters(
+                   new ECDomainParameters(
+                       ecParameters.Curve,
+                       ecParameters.G,
+                       ecParameters.N,
+                       ecParameters.H,
+                       ecParameters.GetSeed()
+                   ),
+                   new SecureRandom()
+               ));
 
-var keyPair     = generator.GenerateKeyPair();
-var privateKey  = (keyPair.Private as ECPrivateKeyParameters).D.ToByteArray().ToBase64();
-var publicKey   = (keyPair.Public  as ECPublicKeyParameters). Q.GetEncoded(). ToBase64();
+var keyPair       = generator.GenerateKeyPair();
+var privateKey    = (keyPair.Private as ECPrivateKeyParameters).D.ToByteArray().ToBase64();
+var publicKey     = (keyPair.Public  as ECPublicKeyParameters). Q.GetEncoded(). ToBase64();
 
 
 
